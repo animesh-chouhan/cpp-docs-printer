b9cff3b5aecb7c79ed5db5ac8032a89e8f98d0f8
Animesh Chouhan <animeshsingh.iitkgp@gmail.com>
2020-03-11 19:38:00 +0530

    added error handling

--- cplusplus.py
+++ cplusplus.py
@@ -11,29 +11,34 @@ from weasyprint import HTML, CSS
 def get_html(url):
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-    tag = soup.find(id="I_main1")
-    body = tag.wrap(soup.new_tag("body")).prettify()
+    tag = soup.find(id="I_main")
 
-    head = """ 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>C++ Reference</title>
-        <link rel="canonical" href="http://www.cplusplus.com/reference/cstdio/printf/">
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-        <link rel="stylesheet" type="text/css" href="../css/main.css">
-    </head>
-    """
+    if tag == None:
+        print("Something went wrong.")
+        return None
+    else:
+        body = tag.wrap(soup.new_tag("body")).prettify()
 
-    html = "<html>" + head + body + "</html>"
-    # print(html)
+        head = """ 
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>C++ Reference</title>
+            <link rel="canonical" href="http://www.cplusplus.com/reference/cstdio/printf/">
+            <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+            <link rel="stylesheet" type="text/css" href="../css/main.css">
+        </head>
+        """
 
-    filename = urlparse(url)[2][1:-1].replace("/", "-")
-    # print(filename)
+        html = "<html>" + head + body + "</html>"
+        # print(html)
 
-    with open("./html/{}.html".format(filename), "w") as f:
-        f.write(html)
+        filename = urlparse(url)[2][1:-1].replace("/", "-")
+        # print(filename)
 
-    return filename
+        with open("./html/{}.html".format(filename), "w") as f:
+            f.write(html)
+
+        return filename
 
 
 def get_pdf(filename):
@@ -44,4 +49,5 @@ def get_pdf(filename):
 
 if __name__ == "__main__":
     filename = get_html(sys.argv[1])
-    get_pdf(filename)
+    if filename != None:
+        get_pdf(filename)
